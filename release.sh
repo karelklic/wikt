0d1a2ec6640a9ff99cfa2702dedae411eb8fc721
Karel Klíč <karelklic@gmail.com>
2009-07-26 21:04:00 +0200

    Releases

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ files=`find data/*.txt -type f`
 files+=" AUTHORS COPYING Makefile RELEASE-NOTES Makefile.Debug Makefile.Release wikt.pro wikt.qrc release.sh"
 files+=" docs/Doxyfile docs/.gitignore docs/index.html docs/FrontPage.dox"
 files+=" .settings .project"
-files+=" images src"
+files+=" images src debian data/enwiktionary-20090711.errata"
 cp --verbose -r --parents $files $tmpdir
 
 DIR=`pwd`
